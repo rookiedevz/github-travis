bf849c026b8936ede77a659c0349eafdf9d5d281
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-28 15:29:04 +0000

    Deploy rookiedevz/github-travis to github.com/rookiedevz/github-travis.git:gh-pages

---
+++ README.md
@@ -0,0 +1,16 @@
+# github-travis
+[![Build Status](https://travis-ci.com/rookiezhoux/github-travis.svg?branch=master)](https://travis-ci.com/rookiezhoux/github-travis)
+
+## Format
+```shell
+./mvnw spring-javaformat:apply
+```
+
+## Build
+```shell
+./mvnw clean package -U
+```
+
+## Run
+```shell
+./mvnw spring-boot:run
---
+++ service/src/main/java/net/gittab/githubtravis/Application.java
@@ -0,0 +1,18 @@
+package net.gittab.githubtravis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * activity Application.
+ *
+ * @author xiaohua zhou
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
