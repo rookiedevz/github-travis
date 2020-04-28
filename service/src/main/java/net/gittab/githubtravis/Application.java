83687eda61c740b1638a8dc49268940ee5022c48
xiaohuazhou <xiaohuazhou@500px.com>
2020-04-28 18:07:10 +0800

    init

---
+++ service/src/main/java/net/gittab/githubtravis/Application.java
@@ -0,0 +1,22 @@
+package net.gittab.githubtravis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * activity Application.
+ *
+ * @author xiaohua zhou
+ */
+@Slf4j
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+
+}
