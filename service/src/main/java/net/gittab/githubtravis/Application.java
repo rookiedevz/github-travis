7e147db02f6a2b2b60fb6fa4ae978b79f0731916
xiaohuazhou <xiaohuazhou@500px.com>
2020-04-28 18:53:38 +0800

    update travis config

--- service/src/main/java/net/gittab/githubtravis/Application.java
+++ service/src/main/java/net/gittab/githubtravis/Application.java
@@ -3,14 +3,11 @@ package net.gittab.githubtravis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import lombok.extern.slf4j.Slf4j;
-
 /**
  * activity Application.
  *
  * @author xiaohua zhou
  */
-@Slf4j
 @SpringBootApplication
 public class Application {
 
